1c5a02e1a7cd348c54859ce2bf11c95c895353a3
JN87 <36296688+jhlee7732@users.noreply.github.com>
2018-10-20 16:15:23 +0900

    renew files

--- js/index.js
+++ js/index.js
@@ -7,16 +7,27 @@ $(document).ready(function() {
     $('.loading').ajaxStop(function() {
         $(this).fadeOut(500);
     });
-    // header 슬라이드
+    // header & category_group 고정(슬라이드)
     var lastScrollTop = 0, delta = 15;
     $(window).scroll(function(event) {
         var st = $(this).scrollTop();
+        var gnbHeight = $('.gnb_container').height();
+        var bannerHeight = $('.main_visual').outerHeight(true);
+        var catHeight = $('.category_group').height();
+        var fixHeight = (bannerHeight + catHeight);
+        if(st >= (fixHeight + 30)) {
+            $('.category_group').addClass('fixedTop');
+        } else if(st < (fixHeight - 30)) {
+            $('.category_group').removeClass('fixedTop');
+        }
         if(Math.abs(lastScrollTop - st) <= delta)
             return;
         if((st > lastScrollTop) && (lastScrollTop > 48)) {
-            $('.gnb_container').css('top', '-50px');
+            $('.gnb_container').css('top', - gnbHeight);
+            $('.category_group').css('top', '0');
         } else {
-            $('.gnb_container').css('top', '0px');
+            $('.gnb_container').css('top', '0');
+            $('.category_group').css('top', gnbHeight);
         }
         lastScrollTop = st;
     });
@@ -36,6 +47,11 @@ $(document).ready(function() {
             $('button.close').toggleClass('menu close');
         };
     });
+    // item에 필터명 추가
+    $('.works li.category:contains("웹디자인")').closest('li.item').addClass('cat1');
+    $('.works li.category:contains("비주얼그래픽")').closest('li.item').addClass('cat2');
+    $('.works li.category:contains("3D 모델링")').closest('li.item').addClass('cat3');
+    $('.works li.category:contains("일러스트")').closest('li.item').addClass('cat4');
     // 맨 위로 가기
     $('.scroll_top').insertBefore('footer');
     $(window).scroll(function() {
